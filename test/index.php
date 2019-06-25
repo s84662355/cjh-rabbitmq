29e08a48049b2476a6a0bcd374ed6af9f2dfb537
陈嘉豪 <271375247@qq.com>
2019-06-25 09:30:25 +0800

    sfsf

--- test/index.php
+++ test/index.php
@@ -5,3 +5,19 @@
  * Date: 2019-06-22
  * Time: 14:29
  */
+require('../vendor/autoload.php');
+
+use CustomRabbitmq\RabbitmqJob;
+use CustomRabbitmq\RabbitmqDriver;
+
+$config = include '../config/rabbitmq_job';
+
+$job = new RabbitmqJob($config);
+
+
+$job->send("fsfds");
+
+
+
+
+

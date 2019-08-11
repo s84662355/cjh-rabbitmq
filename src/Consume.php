03537c31e6770e7a83bc794512382bc31b74c036
陈嘉豪 <271375247@qq.com>
2019-08-11 18:56:48 +0800

    a

--- src/Consume.php
+++ src/Consume.php
@@ -45,10 +45,18 @@ class Consume{
 	public function process_message(AMQPMessage $msg)
 	{
 		$res = AbstractConsume::ACK;
+
+		$log_str = '';
+
         try{
 
             $body = $msg->getBody();
             $body = json_decode($body,true);
+
+            $body_data =  base64_decode($body['body']);
+
+            $log_str+= $body_data;
+
 
 
             if(!empty( $body['message_id']) && !empty($this->redis))
@@ -67,12 +75,19 @@ class Consume{
                 $this->redis->expire($message_key, 200);
             }
 
-            $res = call_user_func_array([$this->callback,'process_message'],[base64_decode($body['body']),$body['config']]);
+            $res = call_user_func_array([$this->callback,'process_message'],[ $body_data,$body['config']]);
         }catch (\Exception $e)
         {
+            $log_str+=$e->getTraceAsString();
+            $log_str+=$e->getLine();
+            $log_str+=$e->getFile();
+
             $res = AbstractConsume::REJECT;
         }catch (\Throwable $throwable)
         {
+            $log_str+=$throwable->getTraceAsString();
+            $log_str+=$throwable->getLine();
+
             $res = AbstractConsume::REJECT;
         }catch (\ParseError $parseError)
         {
@@ -83,15 +98,18 @@ class Consume{
         }
 
 
+
         if($res == AbstractConsume::ACK)
         {
-           ///出列
+            ///出列
             $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
         }else if($res == AbstractConsume::REJECT){
 
         }else if($res == AbstractConsume::CANCEL){
             $msg->delivery_info['channel']->basic_cancel($msg->delivery_info['consumer_tag']);
         }
+
+
 
 	}
 }

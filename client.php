d88ebc8743c4a54b1c129ec8d6e8b3b36c1a35a8
chenjiahao <271375247@qq.com>
2019-08-11 23:34:44 +0800

    a

---
+++ client.php
@@ -0,0 +1,50 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+class FibonacciRpcClient
+{
+    private $connection;
+    private $channel;
+    private $callback_queue;
+    private $response;
+    private $corr_id;
+
+    public function __construct() {
+        $this->connection = new AMQPStreamConnection(
+            '47.112.128.19', 5672, 'guest', '123456');
+        $this->channel = $this->connection->channel();
+        list($this->callback_queue, ,) = $this->channel->queue_declare(
+            "", false, false, true, false);
+        $this->channel->basic_consume(
+            $this->callback_queue, '', false, false, false, false,
+            array($this, 'on_response'));
+    }
+    public function on_response($rep) {
+        if($rep->get('correlation_id') == $this->corr_id) {
+            $this->response = $rep->body;
+        }
+    }
+
+    public function call($n) {
+        $this->response = null;
+        $this->corr_id = uniqid();
+
+        $msg = new AMQPMessage(
+            (string) $n,
+            array('correlation_id' => $this->corr_id,
+                  'reply_to' => $this->callback_queue)
+            );
+        $this->channel->basic_publish($msg, '', 'rpc_queue1');
+        while(!$this->response) {
+            $this->channel->wait();
+        }
+        return intval($this->response);
+    }
+}
+$fibonacci_rpc = new FibonacciRpcClient();
+$response = $fibonacci_rpc->call(30);
+echo " [.] Got ", $response, "\n";
+
+$response = $fibonacci_rpc->call(50);
+echo " [.] Got ", $response, "\n";
